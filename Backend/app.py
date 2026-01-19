c6a4d3093b194f0f64b426b702f51857b0bd3147
Nathan <nathanaladebakare@gmail.com>
2026-01-19 21:32:25 +0000

    Working version of -get_bustops-

--- Backend/app.py
+++ Backend/app.py
@@ -7,30 +7,45 @@ CORS(app)
 
 APP_KEY = "aa273bb9b34546c1be6e3640c78dd5d4"
 
-@app.route("/")
+@app.route("/stops/<lat>/<lon>", strict_slashes=False)
 
 def get_busstops(lat, lon):
-    lat = 51.51679
-    lon = -0.012329
+
     url = "https://api.tfl.gov.uk/StopPoint"
-    parameters = {
-                    "lat": lat,
-                    "lon": lon,
-                    "stopTypes": "NaptamPublicBusCoachTram",
-                    "app_key": APP_KEY                    
-                 }
-    response = requests.get(url, params=parameters)
+
+    params = {
+        "lat": float(lat),
+        "lon": float(lon),
+        "stopTypes": "NaptanPublicBusCoachTram",
+        "app_key": APP_KEY
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        return jsonify({
+            "error": "TfL API error",
+            "status": response.status_code,
+            "body": response.text
+        }), 500
+
     data = response.json()
 
-    stops = []
-    for stop in data.get("stopPoints", []):
-        stops.append({"id":stop.get("id"),"name": stop.get("commonName")})
+    stops = [
+        {
+            "id": stop.get("id"),
+            "name": stop.get("commonName")
+        }
+        for stop in data.get("stopPoints", [])
+    ]
 
     return jsonify(stops)
+
+
+@app.route("/")
 
 def home():
     return "Flask is running"
 
 if __name__ == "__main__":
-    home()
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=False)
